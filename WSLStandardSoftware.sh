d875ee2d0d33b3d226c67163ac62beeda557dccb
Michiel Van Herreweghe <michiel.vh@outlook.com>
2020-04-30 14:24:29 +0200

    Modified WSLStandardSoftware.sh and bashrc-alias

--- WSLStandardSoftware.sh
+++ WSLStandardSoftware.sh
@@ -5,3 +5,6 @@ sudo apt install git -y;
 sudo apt install python3 -y;
 sudo apt install python3-distutils -y;
 sudo apt install python3-venv -y;
+sudo apt install curl -y;
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash;
+npm install -g @vue/cli;
